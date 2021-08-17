090b41fab1f4b66d276b0c3a332ad843d7512f16
username-Hello <nikitos.burak@gmail.com>
2021-08-17 21:38:28 +0300

    add web data model

---
+++ src/main/java/com/beerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package com.beerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
