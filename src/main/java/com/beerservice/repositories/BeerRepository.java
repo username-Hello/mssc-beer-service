d10772ad902042e4d9f3d57dc8363c32b6e302fd
username-Hello <niktos.burak@gmail.com>
2021-11-06 15:08:52 +0300

    adding beer repository

---
+++ src/main/java/com/beerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.beerservice.repositories;
+
+import com.beerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID>{
+
+}
