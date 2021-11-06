c138545bb100e528ece58440f65334642e212eae
username-Hello <niktos.burak@gmail.com>
2021-11-06 15:17:06 +0300

    added validation

--- src/main/java/com/beerservice/web/model/BeerDto.java
+++ src/main/java/com/beerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -15,13 +19,31 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String name;
+
+    @NotNull
     private BeerStyleEnum style;
+
+    @NotNull
+    @Positive
     private Long upc;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
+
     private Integer quantityOnHand;
 }
